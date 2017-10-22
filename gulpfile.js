6e18b6c84c92d0b7a588619dab6cc00257a49da0
Grant Zargaryan <grantzarg@gmail.com>
2017-10-22 22:53:41 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+var gulp = require('gulp');
+
+	sass = require('gulp-sass');
+	browserSync = require('browser-sync');
+    plumber = require('gulp-plumber');
+    del = require('del');
+    imagemin    = require('gulp-imagemin'); 
+    pngquant    = require('imagemin-pngquant');
+    autoprefixer = require('gulp-autoprefixer');
+    csso = require('gulp-csso'); 
+
+
+gulp.task('sass', function(){ 
+    return gulp.src('app/sass/**/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))  
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true})) 
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function() {
+    gulp.watch('app/sass/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload); 
+});
+
+gulp.task('browser-sync', function() { 
+    browserSync({ 
+        server: { 
+            baseDir: 'app' 
+        }
+        
+    });
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+
+gulp.task('img', function() {
+    return gulp.src('app/img/**/*') // Берем все изображения из app
+        .pipe(imagemin({ // Сжимаем их с наилучшими настройками
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+});
+
+gulp.task('build', ['clean', 'img', 'sass'], function() {
+
+    var buildCss = gulp.src([ 
+        'app/css/main.css'
+        ])
+    .pipe(csso())
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('dist/js'))
+
+    var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('clear', function () {
+    return cache.clearAll();
+})
+
+gulp.task('default', ['watch']);
